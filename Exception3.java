f75fa178737de12ec9a4c00570e8578973389a62
Prapti Bhosale <praptibhosale1107@gmail.com>
2026-04-17 10:41:52 +0530

    Add files via upload

---
+++ Exception3.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Exception3
+{
+     public static void main(String args[]) throws ClassNotFoundException
+      {  
+       try
+        {
+         throw new IOException();
+        }
+       catch(Exception e1)
+        {
+   
+        }
+      }
+}
