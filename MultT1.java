ba1d774dcc307a17fbf8364fb3ea91a6edd32c2e
Prapti Bhosale <praptibhosale1107@gmail.com>
2026-05-04 18:31:11 +0530

    Add files via upload

---
+++ MultT1.java
@@ -0,0 +1,37 @@
+
+lass Process1 implements Runnable
+{
+    public void run()
+    {
+      int i;
+       for(i=1;i<=10;i++)
+         {
+
+           System.out.println("P1:"+i);
+          }
+    }
+} 
+class Process2 implements Runnable
+{
+   public void run()
+    {
+      int i;
+       for(i=1;i<=10;i++)
+         {
+
+           System.out.println("P2:"+i);
+          }
+    }
+} 
+ class MultT1
+{
+   public static void main(String args[])
+    {
+       Process1 p1=new Process1();
+       Process2 p2=new Process2();
+       Thread t1=new Thread(p1);
+       Thread t2=new Thread(p2);
+        t1.start();
+        t2.start();
+    }
+}
