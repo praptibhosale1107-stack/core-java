cd15d8d69f8de7c9f9e6c0f4531eea0bd41df175
Prapti Bhosale <praptibhosale1107@gmail.com>
2026-05-05 13:26:45 +0530

    Add files via upload

---
+++ oneplus.java
@@ -0,0 +1,11 @@
+package Rahul;
+import Anjali.samsang;
+class oneplus
+{
+    public static void main (String args[])
+    {
+        samsang s1=new samsang();
+        s1.fun1();
+        s1.fun2(); 
+     } 
+}
