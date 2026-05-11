2b6dee57677a3efdcd885d365564c706a70029fb
Prapti Bhosale <praptibhosale1107@gmail.com>
2026-05-11 20:46:57 +0530

    Add files via upload

---
+++ samsang.java
@@ -0,0 +1,16 @@
+package Anjali;
+public class samsang
+{
+   int mic;
+   int speaker;
+   public void fun1()
+   {
+      mic=2;
+      speaker=5;
+   }
+   public void fun2()
+   {
+     System.out.println(mic);
+     System.out.println(speaker);
+   }
+ }
